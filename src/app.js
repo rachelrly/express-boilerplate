f8b05b84a892266775f439052dab20d46060d024
Rachel Reilly <r.a.reill18@gmail.com>
2020-12-18 15:19:35 -0600

    changed morgan to common

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config')
-const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
 
-app.use(morgan(morganOption))
+const app = express();
+
+app.use(morgan("common"));
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
