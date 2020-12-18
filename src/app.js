20316be697f40ea6245deb1affd99e3bfe266a4e
Rachel Reilly <r.a.reill18@gmail.com>
2020-12-18 15:21:32 -0600

    helmet added to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-const app = express()
+const app = express();
+app.use(helmet());
+
 
 const schema = buildSchema(`
 type Query {
@@ -14,7 +17,7 @@ const root = {
     hello: () => {
         return 'Hello world!'
     }
-}
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
